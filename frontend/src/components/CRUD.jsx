442cfb8f26aecc1b944fac3f7a4b94cacecc205b
brant-skywalker <1773159722@qq.com>
2022-12-02 14:59:44 -0600

    Added current user display.

--- frontend/src/components/CRUD.jsx
+++ frontend/src/components/CRUD.jsx
@@ -257,6 +257,7 @@ function CRUD() {
             <h2>User Login/Register</h2>
             <h1>{LoggedIn ? <img src={require("./yes.png")} width={"100px"} alt="Yes"/> :
                 <img src={require("./no.png")} width={"100px"} alt="No"/>}</h1>
+            <h1>Current User: {UserID !== '' ? UserID : "N/A"}</h1>
             <div className='form'>
                 <input type="text" placeholder="Username" onChange={(e) => {
                     setUsername(e.target.value)
