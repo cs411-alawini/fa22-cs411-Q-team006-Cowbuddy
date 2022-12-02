1334d81f049a75f13bb1849d9600e360fdb7be9b
brant-skywalker <1773159722@qq.com>
2022-12-02 00:21:29 -0600

    Integrated currently not working CRUD interfaces for user operations. Adjusted style.

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,163 @@ app.get("/api/search/:ArticleID", (require, response) => {
         response.send(result);
     });
 });
+app.post("/api/insertUser/", (require, response) => { // Create New Account
+    const Username = require.body.Username;
+    const Email = require.body.Email;
+    const Password = require.body.Password;
+    if (Username && Email && Password) { // Checking to make sure nothing happens when not all inputs filled
+        const fquery = "SELECT max(UserID) FROM `Users`";
+        const sqlSelect = "INSERT INTO `Users` VALUES (?,?,?,?,?,?,?)";
+        var todayDate = new Date().toISOString().slice(0, 10);
+        db.query(fquery, (err2, result2) => {
+            db.query(sqlSelect, [result2 + 1, Email, Username, Password, 0, 0, todayDate], (err, result) => {
+                response.send(result2 + 1); // Sends back UserID
+            });
+        });
+    } else {
+        response.send("Not all inputs populated");
+    }
+
+});
+
+app.get("/api/user/", (require, response) => { // Login Process
+    const Username = require.body.Username;
+    const Email = require.body.Email;
+    const Password = require.body.Password;
+    if (Username && Email && Password) {
+        const sqlSelect = "SELECT UserID FROM `Users` WHERE `Username` = ? and `Email` = ? and `Password` = ?";
+        db.query(sqlSelect, [Username, Email, Password], (err, result) => {
+            response.send(result); // Sends back UserID
+        });
+    } else {
+        response.send("Not all inputs populated");
+    }
+});
+
+app.post("/api/like/", (require, response) => {
+    const UserID = require.body.UserID;
+    const ArticleID = require.body.ArticleID;
+
+    if (UserID) { // Checking to make sure nothing happens when not all inputs filled
+        if (ArticleID) {
+            const sqlUpdate = "UPDATE `Articles` SET `Likes` = `Likes` + 1 WHERE `ArticleID` = ?";
+            const sqlSelect = "UPDATE `Users` SET `ArticlesLiked` = `ArticlesLiked` + 1 WHERE `UserID` = ?";
+            db.query(sqlUpdate, ArticleID, (err, result) => {
+                db.query(sqlSelect, UserID, (err2, result2) => {
+                    response.send("Liked Article");
+                });
+            });
+        } else {
+            response.send("You haven't chosen an Article!");
+        }
+    } else {
+        response.send("You haven't logged in!");
+    }
+
+});
+
+app.post("/api/dislike/", (require, response) => {
+    const UserID = require.body.UserID;
+    const ArticleID = require.body.ArticleID;
+
+    if (UserID) { // Checking to make sure nothing happens when not all inputs filled
+        if (ArticleID) {
+            const sqlUpdate = "UPDATE `Articles` SET `Dislikes` = `Dislikes` + 1 WHERE `ArticleID` = ?";
+            db.query(sqlUpdate, ArticleID, (err, result) => {
+                response.send("Disliked Article");
+            });
+        } else {
+            response.send("You haven't chosen an Article!");
+        }
+    } else {
+        response.send("You haven't logged in!");
+    }
+
+});
+
+app.post("/api/undoDislike/", (require, response) => {
+    const UserID = require.body.UserID;
+    const ArticleID = require.body.ArticleID;
+
+    if (UserID) { // Checking to make sure nothing happens when not all inputs filled
+        if (ArticleID) {
+            const sqlUpdate = "UPDATE `Articles` SET `Dislikes` = `Dislikes` - 1 WHERE `ArticleID` = ?";
+            db.query(sqlUpdate, ArticleID, (err, result) => {
+                response.send("Un-Disliked Article");
+            });
+        } else {
+            response.send("You haven't chosen an Article!");
+        }
+    } else {
+        response.send("You haven't logged in!");
+    }
+
+});
+
+app.post("/api/undoLike/", (require, response) => {
+    const UserID = require.body.UserID;
+    const ArticleID = require.body.ArticleID;
+
+    if (UserID) { // Checking to make sure nothing happens when not all inputs filled
+        if (ArticleID) {
+            const sqlUpdate = "UPDATE `Articles` SET `Likes` = `Likes` - 1 WHERE `ArticleID` = ?";
+            const sqlSelect = "UPDATE `Users` SET `ArticlesLiked` = `ArticlesLiked` - 1 WHERE `UserID` = ?";
+            db.query(sqlUpdate, ArticleID, (err, result) => {
+                db.query(sqlSelect, UserID, (err2, result2) => {
+                    response.send("Un-Liked Article");
+                });
+            });
+        } else {
+            response.send("You haven't chosen an Article!");
+        }
+    } else {
+        response.send("You haven't logged in!");
+    }
+
+});
+
+app.post("/api/comment/", (require, response) => {
+    const UserID = require.body.UserID;
+    const ArticleID = require.body.ArticleID;
+    const Content = require.body.Content;
+    //const DateCommented = require.body.DateCommented;
+
+    if (UserID) { // Checking to make sure nothing happens when not all inputs filled
+        if (ArticleID) {
+            var todayDate = new Date().toISOString().slice(0, 10);
+            const sqlUpdate = "INSERT INTO `Comments` VALUES (?,?,?,?)";
+            db.query(sqlUpdate, [UserID, ArticleID, Content, todayDate], (err, result) => {
+                response.send("Comment Successful!")
+            });
+        } else {
+            response.send("You haven't chosen an Article!");
+        }
+    } else {
+        response.send("You haven't logged in!");
+    }
+
+});
+
+app.post("/api/uncomment/", (require, response) => {
+    const UserID = require.body.UserID;
+    const ArticleID = require.body.ArticleID;
+    const Content = require.body.Content;
+
+    if (UserID) { // Checking to make sure nothing happens when not all inputs filled
+        if (ArticleID) {
+            const sqlUpdate = "DELETE FROM `Comments` WHERE `UserID` = ?, `ArticleID` = ?, `Content` = ?";
+            db.query(sqlUpdate, [UserID, ArticleID, Content], (err, result) => {
+                response.send("Comment Sucessfully Deleted!")
+            });
+        } else {
+            response.send("You haven't chosen an Article!");
+        }
+    } else {
+        response.send("You haven't logged in!");
+    }
+
+});
+
 
 app.get("/api/adv1", (require, response) => {
     const sqlAdv1 = "SELECT DISTINCT ArticleID, Title, DateLiked, Likes " +
